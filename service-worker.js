8c30fa706b15a5184b188487bffbc7c4813d77dc
kimjohsch <kimjohsch@gmail.com>
2026-03-20 18:11:37 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,3 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('v1').then(c=>c.addAll(['fiberkalkyl.html','manifest.json','icon.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
+self.addEventListener('install',e=>{e.waitUntil(caches.open('v3').then(c=>c.addAll(['index.html','manifest.json','icon.png'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
